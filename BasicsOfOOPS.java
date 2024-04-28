1134b34e652fb2e0d0ac2043bd745fef04bf07b4
yeswanth <yeswanthchunchu@gmail.com>
2024-04-28 14:43:34 +0530

    BasicsOfOOPS.java file update

---
+++ BasicsOfOOPS.java
@@ -0,0 +1,31 @@
+public class BasicsOfOOPS {
+    //Using the OOPS we can solve the real world problems
+    //While working with oops principles we should do few things : First we should identify the objects(objects refers to an real world entity)
+    //The objects should have two things : HAS : Attributes(Technically variables) and DOES : Behaviour(Technically methods)
+    //To represent an object, we should have blueprint of the objcte(Technically we call it as class)
+    //Take an example of student : [HAS => Id, name, height, age](variables), [Does => study, play, drink](Behaviour)
+    //class name should follow : "PascalConvention"
+    //Variables and methods should follow : "camelCase"
+    //We use "new" keyword to create an object
+
+    //JVM data areas : method area, stack area, heap area, PC registers, native method area
+    //Method area : .class file
+    //Stack area  : local variables
+    //heap area   : instance variables[objects]
+    //PC registers : address of next instruction should be executed
+    //native method area : code of other languages which was required by java will be available here
+
+    //When we are using the new keyword it is signal to the jvm to create some space to the object
+    //After this the "hashCode" will be give and will collect by the reference variable
+    //hashCode : it will give reference of the object and it will store in the variable
+
+    //Types of variables :
+        //Primitive variables : variables which holds primitive data
+        //Reference variables : variables which holds reference of an object
+        //Instance variables  : Inside a class, outside the methods of class are called instance variables or value of variables changes from object to object
+        //Local variables
+
+    //When we create an object that time, memory will be created for the instance variables
+}
+
+
