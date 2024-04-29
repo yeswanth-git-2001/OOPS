d5d952ea48f94d9f7b08806f56b1a70494c09c1e
yeswanth <yeswanthchunchu@gmail.com>
2024-04-29 09:49:57 +0530

    BasicsOfOOPS.java file update

--- BasicsOfOOPS.java
+++ BasicsOfOOPS.java
@@ -12,18 +12,18 @@ public class BasicsOfOOPS {
     //Method area : .class file
     //Stack area  : local variables
     //heap area   : instance variables[objects]
-    //PC registers : address of next instruction should be executed
+    //PC registers : address of next instruction should be placed here
     //native method area : code of other languages which was required by java will be available here
 
-    //When we are using the new keyword it is signal to the jvm to create some space to the object
-    //After this the "hashCode" will be give and will collect by the reference variable
+    //When we are using the new keyword it is signal to the jvm to create some space to the object in heap area
+    //After this the "hashCode" will be given and will collect by the reference variable
     //hashCode : it will give reference of the object and it will store in the variable
 
     //Types of variables :
         //Primitive variables : variables which holds primitive data
         //Reference variables : variables which holds reference of an object
         //Instance variables  : Inside a class, outside the methods of class are called instance variables or value of variables changes from object to object
-        //Local variables
+        //Local variables     : The variables which is declared inside a methods are known as local variables.
 
     //When we create an object that time, memory will be created for the instance variables
 }
